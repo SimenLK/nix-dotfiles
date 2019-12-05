c3c1bfbb085ab7e4b11a092df98235ec7f683953
Jonas Juselius <jonas.juselius@itpartner.no>
2019-12-05 08:51:05 +0100

    Enable haskell, hie from cachix

--- config/nixpkgs/packages.nix
+++ config/nixpkgs/packages.nix
@@ -1,8 +1,8 @@
 { pkgs, options }:
 with pkgs;
 let
-  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-  hie = all-hies.selection { selector = p: { inherit (p) ghc865; }; };
+  # all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+  # hie = all-hies.selection { selector = p: { inherit (p) ghc865; }; };
   sys = [
     dpkg
     cryptsetup
@@ -134,19 +134,20 @@ let
     haskellPackages.yeganesh
     xmobar
     dmenu
+    zoom-us
   ];
   haskell = if ! options.haskell then [] else with haskellPackages; [
     ghc
     stack
-    # hie
-    # cabal-install
+    hie
+    cabal-install
+    hlint
+    hoogle
     # cabal2nix
     # alex
     # happy
     # cpphs
     # hscolour
-    # hlint
-    # hoogle
     # haddock
     # pointfree
     # pointful
@@ -155,14 +156,6 @@ let
     # hindent
     # codex
     # hscope
-    # parallel
-    # aeson
-    # split
-    # tasty
-    # tasty-hunit
-    # tasty-smallcheck
-    # contravariant
-    # profunctors
     # glirc
   ];
   dotnet = if ! options.dotnet then [] else with dotnetPackages; [
