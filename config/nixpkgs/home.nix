ea148e006470426e50cf78a4ff710a502f37cac3
Jonas Juselius <jonas.juselius@itpartner.no>
2020-03-11 13:14:25 +0100

    Move ssh and git configs to options

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -3,14 +3,6 @@ with pkgs.lib;
 let
   options = import ./options.nix;
 
-  gitUser = {
-    userEmail = "jonas.juselius@itpartner.no";
-    userName = "Jonas Juselius";
-    signing = {
-      key = "jonas.juselius@gmail.com";
-    };
-  };
-
   privateFiles = if ! options.desktop.enable then {} else {
     ssh = {
       source = ~/.dotfiles/ssh;
@@ -29,61 +21,8 @@ let
     forwardAgent = true;
     serverAliveInterval = 30;
     extraConfig = "IPQoS throughput";
-    matchBlocks = {
-      "k?-?" = {
-        user = "admin";
-        hostname = "%h.itpartner.no";
-      };
-      xor = {
-        hostname = "xor.itpartner.no";
-        port = 11022;
-      };
-      xor-intern = {
-        hostname = "xor.itpartner.no";
-        port = 22;
-      };
-      regnekraft = {
-        user = "jonas";
-        hostname = "regnekraft.itpartner.no";
-      };
-      regnekraft-adm = {
-        user = "root";
-        hostname = "regnekraft.itpartner.no";
-      };
-      stallo = {
-        hostname = "stallo.uit.no";
-        serverAliveInterval = 10;
-      };
-      stallo-2 = {
-        hostname = "stallo-login2.uit.no";
-      };
-      stallo-1 = {
-        hostname = "stallo-login1.uit.no";
-      };
-      radon = {
-        hostname = "radon.chem.helsinki.fi";
-      };
-      hep-web01 = {
-        user = "hepadmin";
-        hostname = "10.208.0.130";
-      };
-        # lambda-by-proxy = {
-        #     proxyCommand = "ssh -q jju000@hss.cc.uit.no nc lambda.cc.uit.no 22";
-        # };
-        # "c*-*" = {
-        #     proxyCommand = "ssh -W %h:%p stallo";
-        # };
-        # stallo-forward = {
-        #    hostname = "ssh2.cc.uit.no";
-        #    user = "jju000";
-        #    extraOptions = {
-        #     "PermitLocalCommand" = "yes";
-        #     "LocalCommand" =  "ssh stallo.uit.no";
-        #    };
-        # };
-
-      };
-    };
+    matchBlocks = options.sshHosts;
+  };
 
   extraDotfiles = [
     "aliases"
@@ -241,7 +180,7 @@ in
           sslVerify = false;
         };
       };
-    } // gitUser;
+    } // options.gitUser;
 
     ssh = {
       enable = true;
