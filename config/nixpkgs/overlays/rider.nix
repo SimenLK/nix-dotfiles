1fa59ca2846c758418097f43fcbf34c9109408db
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2022-05-09 14:20:44 +0200

    update rider

--- config/nixpkgs/overlays/rider.nix
+++ config/nixpkgs/overlays/rider.nix
@@ -1,12 +1,12 @@
 self: super:
 {
   rider = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2021.3.2";
+      version = "2021.3.4";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "0arnh9wlw874jqlgad00q0nf1kjp7pvb4xixwrb6v1l9fbr9nsan";
+        sha256 = "0g20wan79qwrm8ml1ns0ma3fcqajjmixzk5sgj0n4dhkzszkq4qj";
       };
   });
 }
