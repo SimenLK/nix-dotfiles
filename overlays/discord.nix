dc30a06cd77256754feccae0f291b3c3174c22d0
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2024-01-23 12:44:23 +0100

    Update discord

--- overlays/discord.nix
+++ overlays/discord.nix
@@ -3,12 +3,12 @@ self: super:
   discord = super.discord.overrideAttrs (attrs: rec {
     # https://discord.com/api/download?platform=linux&format=tar.gz
     # https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz
-    version = "0.0.18";
+    version = "0.0.41";
     name = "discord-${version}";
 
     src = super.fetchurl {
       url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "1hl01rf3l6kblx5v7rwnwms30iz8zw6dwlkjsx2f1iipljgkh5q4";
+      sha256 = "sha256-yzCOlYIDvbhJRbiqf5NC2iBT2ezlJP81O/wtkWIEp+U=";
     };
   });
 }
