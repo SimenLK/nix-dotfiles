a6ebed50fe2ea098e5c921cb2563c50061918458
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2024-07-03 13:51:50 +0200

    Bump ferdium

--- overlays/ferdium.nix
+++ overlays/ferdium.nix
@@ -1,13 +1,13 @@
 self: super:
 let
   arch = "amd64";
-  version = "6.7.2";
+  version = "6.7.5";
 in
 {
   ferdium-old = super.ferdium.overrideAttrs (attrs: {
     src = super.fetchurl {
       url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-${arch}.deb";
-      hash = "sha256-RH8iHqJ0Nd3wkXmv/ZGX5JeWtxyJtVLVb0tz6tnkjrw=";
+      hash = "sha256-PhWYHGRSX9wpPAlfA/UN2nmr++IWbeeEadgU6WAXaTQ=";
     };
   });
 }
