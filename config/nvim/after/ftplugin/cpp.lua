81f288dab3b15adf04821459786ef8713c16f3fe
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2024-07-03 13:52:04 +0200

    Add cpp script to look for build.sh
    If it finds it, replade makeprg with invoking ./build.sh. If you're not
    using a Makefile, where `make` is the default command invoked when using
    the vim :make command

--- config/nvim/after/ftplugin/cpp.lua
+++ config/nvim/after/ftplugin/cpp.lua
@@ -1,3 +1,13 @@
-vim.opt_local.makeprg = "build.bat"
+local function find_build_script()
+  local result = "make"
+
+  if vim.fn.filereadable("build.sh") then
+    result = "./build.sh"
+  end
+
+  return result
+end
+
+vim.opt_local.makeprg = find_build_script()
 vim.opt_local.cino = "(0"
 vim.opt_local.textwidth = 80
