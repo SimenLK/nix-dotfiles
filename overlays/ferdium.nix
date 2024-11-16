f1e5b69b17a23b7bb51b6d68abfc1f85049e3c92
simkir <simen@kirkvik.no>
2024-11-16 13:22:28 +0100

    Update ferdium

--- overlays/ferdium.nix
+++ overlays/ferdium.nix
@@ -1,13 +1,13 @@
 self: super:
 let
   arch = "amd64";
-  version = "6.7.7";
+  version = "7.0.0";
 in
 {
   ferdium = super.ferdium.overrideAttrs {
     src = super.fetchurl {
       url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-${arch}.deb";
-      hash = "sha256-5OW10sABNNYQNUgorM634j5oiBhJJby1ymH6UcmknRg=";
+      hash = "sha256-Ey2FhekQVSYB0ki4U3HyuKs/URgtZMCl41jkPsZMNrQ=";
     };
   };
 }
