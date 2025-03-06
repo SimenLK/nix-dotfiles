6e1556a3e8b7689cd07db9e3f641e9db1acaf82c
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2025-03-06 15:04:02 +0100

    Update rider 2024.3 -> 2024.3.6

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -4,28 +4,13 @@ let
   jetbrains = super.callPackage jetbrainsNix { };
 
   rider-latest = jetbrains.rider.overrideAttrs (attrs: rec {
-    version = "2024.3";
+    version = "2024.3.6";
     name = "rider-${version}";
 
     src = super.fetchurl {
       url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-      sha256 = "sha256-75thwYhRtvPvK/o/4UezSsGRYi9lpB8rU7OmCfm/82A=";
+      sha256 = "sha256-H5258/kMcf5Hbj4XrHi+n8yYLj8BfFmPYxtc1gDm2kM=";
     };
-
-    postInstall = ''
-      cd $out/rider
-
-      ls -d $PWD/plugins/cidr-debugger-plugin/bin/lldb/linux/*/lib/python3.8/lib-dynload/* |
-      xargs patchelf \
-        --replace-needed libssl.so.10 libssl.so \
-        --replace-needed libcrypto.so.10 libcrypto.so \
-        --replace-needed libcrypt.so.1 libcrypt.so
-
-      for dir in lib/ReSharperHost/linux-*; do
-        rm -rf $dir/dotnet
-        ln -s ${super.dotnet-sdk_8.unwrapped}/share/dotnet $dir/dotnet
-      done
-    '';
   });
 in
 {
