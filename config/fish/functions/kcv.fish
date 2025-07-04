c2fa9e3a0ff862c76f74e64d06e5d9d5371831ff
Simen Kirkvik <simen.kirkvik@tromso.serit.no>
2025-07-04 08:45:30 +0200

    Make kcv fish function better

--- config/fish/functions/kcv.fish
+++ config/fish/functions/kcv.fish
@@ -1,3 +1,5 @@
 function kcv --wraps='kubectl config current-context' --description 'alias kcv=kubectl config current-context'
-    kubectl config current-context;
+    set -l ctx (kubectl config current-context)
+
+    kubectl config get-contexts $ctx
 end
